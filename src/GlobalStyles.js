21e72360e028b60e5a6e844fe90e6302ea55caee
mariereese <mariereese12@gmail.com>
2020-11-24 15:52:21 +0100

    Styling of FoodList

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,12 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
+  
   * {
     box-sizing: border-box;
   }
 
   body {
     margin: 0;
-    font-family: sans-serif;
+    font-family: 'Rubik', sans-serif;
   }
 `
