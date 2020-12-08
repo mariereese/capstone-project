cc61598afd764332a07d91b0024b1a96115bf968
mariereese <mariereese12@gmail.com>
2020-12-08 19:07:47 +0100

    Fix Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyle from './styles/GlobalStyle'
@@ -8,9 +8,9 @@ import GlobalStyle from './styles/GlobalStyle'
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
