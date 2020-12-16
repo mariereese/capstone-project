6721a57d231a160dd46b42e4c20962680f08a380
mariereese <mariereese12@gmail.com>
2020-12-16 14:40:58 +0100

    Render Sum of all saved Purchases

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,14 +1,40 @@
 import styled from 'styled-components/macro'
+import { useState, useEffect } from 'react'
 import PageWrapper from '../../components/PageWrapper'
 import Header from '../../components/Header/Header'
 import Navigation from '../../components/Navigation/Navigation'
+import Card from '../../components/Card'
 import { ReactComponent as Plus } from '../../images/plus-icon.svg'
+import loadLocally from '../../lib/loadLocally'
 
 export default function Dashboard() {
+  const [annualFootprintSum, setAnnualFootprintSum] = useState(0)
+  const [allPurchases, setAllPurchases] = useState([])
+
+  useEffect(() => {
+    setAllPurchases(loadLocally('savedPurchase') ?? [])
+  }, [])
+
+  useEffect(() => {
+    const numbers = allPurchases.map(({ sum }) => sum)
+    setAnnualFootprintSum(
+      numbers.reduce((result, number) => {
+        return result + number
+      }, 0)
+    )
+  }, [allPurchases])
+
   return (
     <>
       <Header title="Dashboard" />
-      <PageWrapper></PageWrapper>
+      <PageWrapper>
+        <AnnualFootprintCard>
+          <h2>Dein Jahres Foodprint:</h2>
+          <p>
+            {annualFootprintSum} kg CO<sub>2</sub>
+          </p>
+        </AnnualFootprintCard>
+      </PageWrapper>
       <AppNavigation>
         <Navigation isDisabled icon={<PlusIcon />} />
       </AppNavigation>
@@ -16,11 +42,14 @@ export default function Dashboard() {
   )
 }
 
+const AnnualFootprintCard = styled(Card)``
+
 const AppNavigation = styled.div`
   width: 100%;
   position: fixed;
   bottom: 0;
 `
+
 const PlusIcon = styled(Plus)`
   height: 60%;
   position: absolute;
